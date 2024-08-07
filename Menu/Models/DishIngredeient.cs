c3e217114415e31b22d2b69b4565ae9abc948f91
Johnny Tran <tranjohnny917@gmail.com>
2024-08-07 13:03:26 -0400

    Add project files.

---
+++ Menu/Models/DishIngredeient.cs
@@ -0,0 +1,11 @@
+﻿namespace Menu.Models
+{
+    public class DishIngredeient
+    {
+        public int Id { get; set; }
+        public Dish Dish { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+
+    }
+}
