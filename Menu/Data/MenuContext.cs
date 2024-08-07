dc3c479771fa06067ca49089d5d41dc1585f4743
Johnny Tran <tranjohnny917@gmail.com>
2024-08-07 13:11:53 -0400

    Added 4 classes in Models Folder and new folder named Data

--- Menu/Data/MenuContext.cs
+++ Menu/Data/MenuContext.cs
@@ -1,6 +1,24 @@
-﻿namespace Menu.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+
+namespace Menu.Data
 {
-    public class MenuContext
+    public class MenuContext : DbContext
     {
+        public MenuContext( DbContextOptions<MenuContext> options ) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredeient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Menu/Models/DishIngredeient.cs
+++ Menu/Models/DishIngredeient.cs
@@ -2,7 +2,7 @@
 {
     public class DishIngredeient
     {
-        public int Id { get; set; }
+        public int DishId { get; set; }
         public Dish Dish { get; set; }
         public int IngredientId { get; set; }
         public Ingredient Ingredient { get; set; }
